9ec2b5ec174a5307e3e0a0179335ca25bea56747
jawadiq <jawadiqbal84@gmail.com>
2020-05-18 22:48:21 +0200

    update

--- src/cover/banner.js
+++ src/cover/banner.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './cover.css'
 
 const Banner = () => {
     return (
-        <div>
-            NathalienSelayse
+        <div className="banner">
+        <h1>    Nathalie <br/>Sekayse</h1>
         </div>
     );
 };
