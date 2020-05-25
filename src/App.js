3bf4f6bef8fb0a1d5eef0be0ae9c51ecc058be55
jawadiq <jawadiqbal84@gmail.com>
2020-05-25 16:22:08 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
 import React from "react";
 import Header from "./components/header-footer/header";
 import "./App.css";
-import Nata from "./resources/design.gif";
+import Nata from "./resources/banner.png";
 import Features from "./components/carousel/index";
-import Cover from './cover/index'
-import Footer from './footer/footer';
-import Designer from './components/container/designers/designers'
+import Cover from "./cover/index";
+import Footer from "./footer/footer";
+import Designer from "./components/container/designers/designers";
 
 function App() {
-
-
   return (
-
-
-   
     <div className="container">
-    
-    <Header/>
-      <div
-        className="appmain"
-     
-      >
+      <Header />
+      <div className="appmain">
         <img src={Nata} alt="main" className="image-main"></img>
-       
-      
-        <Cover/>
+
+        <Cover />
       </div>
-      <Features/>
+      <Features />
       {/* <Designer/> */}
-      <Footer/>
-      
+      <Footer />
     </div>
- 
+    
   );
 }
 
--- src/components/container/container.js
+++ src/components/container/container.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = () => {
     return (
-        <div className="Container" style={{height:"3000px",width:"1100px"}} >
+        <div  >
             
         </div>
     );
